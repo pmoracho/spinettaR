0e0225123527358bdf9943b61373f34c891ca08c
Patricio Moracho <pmoracho@gmail.com>
2020-01-23 01:45:26 -0300

    algunas mejoras

--- R/grupos.R
+++ R/grupos.R
@@ -1,11 +1,16 @@
 library(shiny)
+# To do
+# 1. Inicialización de datos
+# 2. Personalizar interface traducir
+# 3. Validar si el id ya existe
 
+grupos.file <- "../data/grupos.Rda"
 if (!exists("dtedit")) {
   source("DTedit.R", encoding = "UTF-8")
 }
 
 get_grupos <- function() {
-  grupos.file <- "grupos.Rda"
+
   if (file.exists(grupos.file)) {
     t_grupos = readRDS(grupos.file)
   } else {
@@ -44,11 +49,12 @@ grupos.validate <- function(operation, olddata, data, row){
   errores_or_warnings <- data.frame(warning=logical(0), msg=character(0), stringsAsFactors = FALSE)
 
   if (is.null(data$nombre[row]) | data$nombre[row]=="") {
-    errores_or_warnings <- rbind(errores_or_warnings, list(warning = FALSE, msg="Debe ingresar un nombre de grupo"))
+    errores_or_warnings <- rbind(errores_or_warnings, data.frame(warning = FALSE, msg="Debe ingresar un nombre de grupo"))
   }
+  str(data)
   print(data$grupo_id[row])
   if (data$grupo_id[row] < 1 ) {
-    errores_or_warnings <- rbind(errores_or_warnings, list(warning = TRUE, msg="El grupo_id es inválido"))
+    errores_or_warnings <- rbind(errores_or_warnings, data.frame(warning = TRUE, msg="El grupo_id es inválido"))
   }
 
   return(errores_or_warnings)
